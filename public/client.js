4760c13dce53b395061c31de8cb5275ee384f50b
Vansh <gu[ptavansh67@gmail.com>
2024-05-22 21:41:22 +0530

    first commit

--- public/client.js
+++ public/client.js
@@ -23,4 +23,60 @@ require(['vs/editor/editor.main'], function() {
             editor.setValue(code);
         }
     });
+
+    const templates = {
+        javascript: [
+            'function x() {',
+            '\tconsole.log("Hello world!");',
+            '}'
+        ].join('\n'),
+        python: [
+            'def x():',
+            '\tprint("Hello world!")'
+        ].join('\n'),
+        java: [
+            'public class Main {',
+            '    public static void main(String[] args) {',
+            '        System.out.println("Hello, World!");',
+            '    }',
+            '}'
+        ].join('\n'),
+        cpp: [
+            '#include <iostream>',
+            'using namespace std;',
+            'int main() {',
+            '    cout << "Hello, World!";',
+            '    return 0;',
+            '}'
+        ].join('\n'),
+        html: [
+            '<!DOCTYPE html>',
+            '<html>',
+            '<head>',
+            '    <title>Hello World</title>',
+            '</head>',
+            '<body>',
+            '    <h1>Hello, World!</h1>',
+            '</body>',
+            '</html>'
+        ].join('\n'),
+        css: [
+            'body {',
+            '    font-family: Arial, sans-serif;',
+            '    background-color: #f0f0f0;',
+            '    margin: 0;',
+            '    padding: 0;',
+            '}',
+            'h1 {',
+            '    color: #333;',
+            '}'
+        ].join('\n')
+    };
+
+    document.getElementById('language-selector').addEventListener('change', function() {
+        var newLanguage = this.value;
+        var template = templates[newLanguage] || '';
+        monaco.editor.setModelLanguage(editor.getModel(), newLanguage);
+        editor.setValue(template);
+    });
 });
